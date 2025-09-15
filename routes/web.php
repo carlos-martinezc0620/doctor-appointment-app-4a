2496f87dd192273e5db474433c1e7b39bed47f69
carlos-martinezc0620 <carlos.martinez@tecdesoftware.edu.mx>
2025-09-15 16:42:59 +0400

    Chore: admin core added

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', 'admin');
+/*
 Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
